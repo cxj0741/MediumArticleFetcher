52c673690833e5255b6b57595236dba7b276555d
cxj <2496819537>
2024-08-11 15:14:57 +0800

    修正

--- fetch.py
+++ fetch.py
@@ -1,4 +1,5 @@
 import asyncio
+import os
 from concurrent.futures import ThreadPoolExecutor
 from playwright.async_api import Page, async_playwright
 import uuid
@@ -190,10 +191,12 @@ async def scrape_article_content_and_images(url, context):
 
 
 async def run(playwright, keyword=None, refresh=False):
+    user_data_dir=os.path.abspath('./User Data')
     try:
         context = await playwright.chromium.launch_persistent_context(
+            user_data_dir=user_data_dir,
             # user_data_dir=r'C:\Users\86157\AppData\Local\Google\Chrome\User Data',
-            user_data_dir=r'D:\pythonProject\MediumArticleFetcher\User Data',
+            # user_data_dir=r'D:\pythonProject\MediumArticleFetcher\User Data',
             # user_data_dir=r'./User Data', user_data_dir=r'D:\pythonProject\MediumArticleFetcher\User Data',
             # user_data_dir=r'./User Data',
             headless=False,
